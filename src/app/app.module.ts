5103f1d16751d671828060bffc478928091c268e
dutr5288 <dutr5288@colorado.edu>
2019-03-14 16:02:33 -0600

    Hot Fixed: Fix the dashboard routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    DashboardComponent
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

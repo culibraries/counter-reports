6aeee7ccf9a35d469ce8ec5a4d3dc49e5a6b9c83
dutr5288 <dutr5288@colorado.edu>
2019-05-31 15:11:12 -0600

    Fixed: Performance issue

--- src/app/core/services/publication.service.ts
+++ src/app/core/services/publication.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable, forkJoin } from 'rxjs';
+import { Observable, forkJoin, empty, of } from 'rxjs';
 import { ApiService } from './api.service';
 import { Publication } from '../models';
-import { map, switchMap } from 'rxjs/operators';
+import { map, expand, concatMap, reduce } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,48 +16,23 @@ export class PublicationService {
       .pipe(map(data => data));
   }
 
-  getByFilters(filterParameterURL: string): any {
-    return this.apiService
-      .get(
-        '/counter/publications/?format=json&page_size=100000&' +
-          filterParameterURL
-      )
-      .pipe(
-        switchMap(data => {
-          if (data['count'] <= 100000) {
-            return data.results;
-          } else {
-            const count = Math.ceil(data['count'] / 100000);
-            return this.getByPageSize(count, filterParameterURL).pipe(
-              map(folkJoinData => {
-                for (let j = 0; j <= count - 2; j++) {
-                  data.results = data.results.concat(
-                    folkJoinData[j]['results']
-                  );
-                }
-                return data.results;
-              })
-            );
-          }
-        })
-      );
-  }
-
-  getByPageSize(count: number, filterParameterURL: string): any {
-    const urls = [];
-
-    for (let i = 2; i <= count; i++) {
-      urls.push(
-        this.apiService
-          .get(
-            '/counter/publications/?format=json&page_size=100000&page=' +
-              i +
-              '&' +
-              filterParameterURL
-          )
-          .pipe(map(data => data))
-      );
-    }
-    return forkJoin(urls);
+  getByPageNext(filterParameterURL: string): Observable<any> {
+    console.log(filterParameterURL);
+    const url =
+      '/counter/publications/?format=json&page_size=100000&' +
+      filterParameterURL;
+    return this.apiService.get(url).pipe(
+      expand((response: any) => {
+        console.log(response);
+        if (response && response.next) {
+          console.log(response.next);
+          return this.apiService.getNextPage(response.next);
+        } else {
+          return empty();
+        }
+      }),
+      map(obj => obj),
+      reduce((acc, x: any) => acc.concat([x.results]), [])
+    );
   }
 }
